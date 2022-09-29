c8e0d21d8fd2c1573e1d0afedb23814e528a9b2d
Yuxuan <nine.slide@gmail.com>
2022-09-29 13:56:15 +0200

    update expenses array

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import Expenses from './components/Expenses';
+import { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
+const init = [
+    {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: 'e2',
+        title: 'New TV',
+        amount: 799.49,
+        date: new Date(2021, 2, 12),
+    },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
 function App() {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: 'e2',
-            title: 'New TV',
-            amount: 799.49,
-            date: new Date(2021, 2, 12),
-        },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+    const [espenses, setExpenses] = useState(init);
     const saveExpenseDateHandler = (expense) => {
-    return(
-        console.log(expense)
-        )
-    }
+        // setExpenses([expense,...espenses])//update depending on the previous state
+
+        setExpenses((prevState) => {
+            return [expense, ...prevState];
+        }); //.adding the new expense to the existing expenses array
+    };
     return (
         <div>
-           <NewExpense onSaveExpenseDate ={saveExpenseDateHandler}/>
-            <Expenses item={expenses} /> 
-            { /*//> item in the Expenses component points at a value the we want to pass into the Expenses component, which is expenses array  */}
-           
+            <NewExpense onSaveExpenseDate={saveExpenseDateHandler} />
+            <Expenses item={espenses} />
+            {/*//> item in the Expenses component points at a value the we want to pass into the Expenses component, which is expenses array  */}
         </div>
     );
 }
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -11,29 +11,29 @@ function Expenses(props) {
     return (
         <div>
             <Card className="expenses">
-            <ExpensesFilter selected = {year} onSelectYear={selectYearHandler} />
-            {/* //.two way binding [selected={year} and in the filter add a new property to select tag] */}
-                <ExpenseItem
-                    title={props.item[0].title}
-                    amount={props.item[0].amount}
-                    date={props.item[0].date}
+                <ExpensesFilter
+                    selected={year}
+                    onSelectYear={selectYearHandler}
                 />
-                {/* (//'using props to get data from parent component, and in the parent component, the data passed in should be specified.eg item = expenses in App.js') */}
+                {/* //.two way binding [selected={year} and in the filter add a new property to select tag] */}
+
+                {props.item.map((expense) => (
+                    <ExpenseItem
+                        title={expense.tltle}
+                        amount={expense.item}
+                        date={expense.date}
+                    />
+                ))}
+                {/*//' use .map to transform array to object to jsx elements 
+                //![item = expenses] in the App.js*/}
+
+                {/* (//:using props to get data from parent component, and in the parent component, the data passed in should be specified.eg item = expenses in App.js')
                 <ExpenseItem
                     title={props.item[1].title}
                     amount={props.item[1].amount}
                     date={props.item[1].date}
                 />
-                <ExpenseItem
-                    title={props.item[2].title}
-                    amount={props.item[2].amount}
-                    date={props.item[2].date}
-                />
-                <ExpenseItem
-                    title={props.item[3].title}
-                    amount={props.item[3].amount}
-                    date={props.item[3].date}
-                />
+                */}
             </Card>
         </div>
     );
